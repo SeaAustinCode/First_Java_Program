48b6d1ba52262cda5ee687e9be22a1dddb34ebdb
SeaAustinCode <seaaustincode@gmail.com>
2022-04-02 17:42:44 -0700

    First Java Program

---
+++ FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) { // public static void - keywords that do the same thing as the function keyword. main = name of method. () will list any parameters.
+        System.out.println("My Name is Austin O'Neil");
+        System.out.println("I am 31 years old");
+        System.out.println("My hometown is Issaquah, WA");
+    }
+}
+
